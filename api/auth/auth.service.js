f12d69619ac1fb9ea42de258786d6edc2c44b825
Arnon Arditi <arnonrdt1234@gmail.com>
2023-03-16 19:07:08 +0200

    buttons disables

--- api/auth/auth.service.js
+++ api/auth/auth.service.js
@@ -2,14 +2,13 @@ const Cryptr = require('cryptr')
 const bcrypt = require('bcrypt')
 const userService = require('../user/user.service')
 const logger = require('../../services/logger.service')
-const cryptr = new Cryptr(process.env.SECRET1 || 'Secret-Puk-1234')
+const cryptr = new Cryptr(process.env.SECRET1 || 'Secret-1234')
 
 async function login(username, password) {
     logger.debug(`auth.service - login with username: ${username}`)
 
     const user = await userService.getByUsername(username)
     if (!user) return Promise.reject('Invalid username or password')
-    // TODO: un-comment for real login
     const match = await bcrypt.compare(password, user.password)
     if (!match) return Promise.reject('Invalid username or password')
 
